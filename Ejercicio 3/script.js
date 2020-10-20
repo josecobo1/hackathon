b7a18525f7f70cdaa0a0d7f010a58f7a0d9f69bb
Jose Cobo <Jose@MacBook-Pro-de-Jose.local>
2020-10-20 16:48:50 +0200

    versión 2

--- Ejercicio 3/script.js
+++ Ejercicio 3/script.js
@@ -1,7 +1,16 @@
+// Seleccionamos el boton del formulario por medio del tag, como solo hay uno uso el atributo TagName
 const button = document.getElementsByTagName("button");
 
+/*
+let formName = document.getElementsByName("name")[0].value;
+let formMail = document.getElementsByName("mail")[0].value;
+let formMessage = document.getElementsByName("message")[0].value;
+*/
+
+// Función que se ejecuta mostrando un aviso en pantalla
 function showAlert(){
     alert('Codigo de validación');
  }
  
+ // EventListener que ejecuta la función showAlert cada vez que se aprieta el formulario
  button[0].addEventListener('click', showAlert); 
