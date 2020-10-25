4ef8c357120c9c38f95d50c070e21d83e5936788
Jose Cobo <Jose@MacBook-Pro-de-Jose.local>
2020-10-25 11:13:32 +0100

    mejoras en script.js

--- Ejercicio 1/script.js
+++ Ejercicio 1/script.js
@@ -1,82 +1,51 @@
-let previousTarget; // Variable que me guarda el último target para quitarle la classe "target"
-let game;
+// Esta variable contiene el id target, de esta manera esta accesible para despues 
+// eliminarlo desde el metodo remove
+let target; 
 
-// Función que me selecciona un id para identificar que target hay que pintar de rojo
-const targetSelector = () => {
-    let position = Math.floor(Math.random() * 9) + 1;
-    //alert(position);
-    return position;
+
+// Si el usuario acierta el div paramos de repetir las funciones
+const win = () => {
+    clearInterval(window.timing);
+    alert("Has ganado");
 }
 
 
+// Funcion que escoge un target y lo pinta
+const setTarget = () => {
+    // Escogemos un objetivo al azar
+    target = Math.floor(Math.random() * 9) + 1;
 
-// Esta función pinta el target a partir del id que devuelve targetSelector()
-const cercleSelector = () => {
+    // Añadimos la classe red
+    document.getElementById(target).classList.add("target");
 
-    // en la variable targetId guardo el id que me devuelve targetSelector()
-    let targetId = targetSelector();
-
-    // Identifico el target actual con el id anterior
-    let currentTarget = document.getElementById(targetId); 
-
-    // Le añado la classe "target" para pintarlo de rojo
-    currentTarget.classList.add("target"); 
-
-    // Le añado un eventListener para poder registrar si se clica para mostrar el mensaje y finalizar el juego
-    endGame(targetId);
-
-    // Guardo el id en la variable previousTarget para despues quitarle la classe "target"
-    previousTarget = targetId; 
-
-    // Esta función se repita en bucle cada 3 segundos y selecciona y des-selecciona cada target
-    game = setInterval(function () {
-
-        // Buscamos el nuevo target
-        let number = targetSelector(); 
-
-        // seleccionamos el target corresponsiente
-        let selectedCercle = document.getElementById(number); 
-
-        // Al target anterior le borro la classe "target"
-        document.getElementById(previousTarget).classList.remove("target"); 
-
-        // guardo en la variable previousTarget el nuevo target para cuando tenga que quitarle la classe
-        previousTarget = number; 
-
-        // pinto de rojo el nuevo target
-        selectedCercle.classList.add("target"); 
-
-        // Al nuevo targt le añado un EventListener para cuando el usuario clique le informe de que ha ganado y se detenga el juego
-        document.getElementById(number).addEventListener('click', function () {
-
-            // Pinto un alert para el usuario
-            alert("Has ganado"); 
-
-            // Si el usuario clica en el circulo correcto se para el juego 
-            clearInterval(game); 
-
-        });
-
-    }, 3000);
-
+    // Al target actual le ponemos un eventlistener
+    document.getElementById(target).addEventListener('click', win);
 }
 
 
-const endGame = (n) => {
+// Al target anterior hay que eliminarle la classe red y quitarle el eventlistener
+const removeTarget = () => {
+    // Eliminamos la classe red
+    document.getElementById(target).classList.remove("target");
 
-    document.getElementById(n).addEventListener('click', function () {
-
-        // Pinto un alert para el usuario
-        alert("Has ganado"); 
-
-        // Si el usuario clica en el circulo correcto se para el juego 
-        clearInterval(game); 
-
-    });
-
-
-
+    // Al target anterior le borro la classe "target"
+    document.getElementById(target).removeEventListener('click', win);
 }
+
+
+// Esta función llama a las funciones set y remove target desde la función setInterval
+const adjustTarget = () => {
+    removeTarget();
+    setTarget();
+}
+
+
+// Al cargar la pagina inicia el juego
+const startGame = () => {
+    setTarget();
+    window.timing = setInterval(adjustTarget, 3*1000);
+}
+
 
 // Ejecuta la función que hace todo el juego
-cercleSelector();
+startGame();
